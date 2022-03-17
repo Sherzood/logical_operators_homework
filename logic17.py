d7787c7309ecb110f181b7c16bd5f688d8a03cd6
sherzod89 <sherzodms@inbox.ru>
2022-03-17 22:32:55 +0300

    01

--- logic17.py
+++ logic17.py
@@ -6,4 +6,12 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    a1=a%10
+    a//=10
+    a2=a%10
+    a//=10
+    a3=a%10
+    a//=10
+    a4=a%10
+    a5=a//10
+    return a1<a2 and a2<a3 and a3<a4 and a4<a5
